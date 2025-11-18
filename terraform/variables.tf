111a1a74db4e1d10914a8fc52ccf8dc6404705d5
kittuhk <harikrishna.bommidi1803@gmail.com>
2025-11-18 15:31:14 +0530

    Update project variable in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,6 @@
-variable "project" {default ="gowtham-project-476511"}
+variable "project" {default ="hari-chandu-anunay"}
 variable "region" { default = "us-central1" }
 variable "zone"   { default = "us-central1-a" }
 variable "ssh_user" { default = "ubuntu" }
+
 
